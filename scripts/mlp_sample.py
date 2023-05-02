e364e70ec926163e8a8f8699323784ae9c329749
Jeremy <jdwurbs@gmail.com>
2023-05-03 01:50:19 +0900

    Add samples

---
+++ scripts/mlp_sample.py
@@ -0,0 +1,22 @@
+from wichi import Neuron, Layer, MLP, draw_dot
+
+
+neuron = Neuron(num_input=2)
+
+x = [2., 3.]
+y = neuron(x)
+print(f'y: {y}')
+
+
+layer = Layer(num_input=5, num_output=3)
+y = layer([-3., 2., 4., -1., 2.5])
+print(f'y: {y}')
+
+
+mlp = MLP(dim=[5, 10, 30, 10, 1])
+y = mlp([-3., 1., 5., -6., 3.])
+print(f'y: {y}')
+
+draw_dot(y).render()
+y.backward()
+draw_dot(y).render()
---
+++ scripts/value_sample.py
@@ -0,0 +1,33 @@
+from wichi import Value, draw_dot
+
+
+class Neuron(object):
+    def __init__(self, w1, w2, b):
+        self.w1 = Value(w1, label='w1')
+        self.w2 = Value(w2, label='w2')
+        self.b = Value(b, label='b')
+
+    def forward(self, x):
+        x1, x2 = x
+        _1 = self.w1 * x1; _1.label = '_1'
+        _2 = self.w2 * x2; _2.label = '_2'
+        _3 = _1 + _2; _3.label = '_3'
+        dp = _3 + self.b; dp.label = 'dp'
+        out = dp.tanh(); out.label = 'tanh'
+        return out
+
+    def __call__(self, x):
+        return self.forward(x)
+
+
+neuron = Neuron(w1=-3., w2=1., b=6.7)
+x1 = Value(2., label='x1')
+x2 = Value(0., label='x2')
+
+y = neuron(x=(x1, x2)); y.label = 'y'
+draw_dot(y).render()  # note that all the gradients will be zero
+
+
+# Backpropagation
+y.backward()
+draw_dot(y).render()  # note that all the gradients will be filled in
