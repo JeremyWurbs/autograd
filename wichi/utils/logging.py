3900dc6a19c13aaca521014c5810a942262034d7
Jeremy <jdwurbs@gmail.com>
2023-05-05 10:31:15 +0900

    Add plot method to logger

--- wichi/utils/logging.py
+++ wichi/utils/logging.py
@@ -1,6 +1,10 @@
+import matplotlib.pyplot as plt
+
+
 class Logger(object):
     def __init__(self):
         self.logged_objects = dict()
+        self.axes = dict()
 
     def log_scalar(self, name, value, step=None):
         if name not in self.logged_objects:
@@ -9,3 +13,18 @@ class Logger(object):
 
     def get_log(self, name):
         return self.logged_objects[name]
+
+    def plot(self, names, title):
+        colors = ['green', 'orange', 'cyan', 'blue', 'magenta', 'red', 'black']
+        if title not in self.axes:
+            fig, ax = plt.subplots()
+            self.axes[title] = ax
+        for idx, name in enumerate(names):
+            steps = [t['step'] for t in self.get_log(name)]
+            losses = [t['value'] for t in self.get_log(name)]
+            self.axes[title].plot(steps, losses, color=colors[idx % len(colors)])
+        self.axes[title].set_title(title)
+        self.axes[title].set_xlabel('step')
+        self.axes[title].legend(names)
+        plt.draw()
+        plt.pause(0.5)
