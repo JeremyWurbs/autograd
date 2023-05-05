c8ae8cd613381ec78886a3759070904f0ef0c4c5
Jeremy <jdwurbs@gmail.com>
2023-05-05 12:55:54 +0900

    Add resource images for readme

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
      name='wichi',
-     version='0.0.1',
+     version='1.0.0',
      description="Package serving as an educational implementation of autograd",
      long_description=long_description,
      long_description_content_type="text/markdown",
